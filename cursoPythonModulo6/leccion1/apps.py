8a8bb9248e480d2f045c56970f6ee36958e52b17
Carlos Cardenas <usr.carlos@gmail.com>
2024-12-02 19:43:04 -0300

    primera subida de archivos

---
+++ cursoPythonModulo6/leccion1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Leccion1Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "leccion1"
---
+++ cursoPythonModulo6/leccion1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+#from django.http import HttpRequest
+
+# Create your views here.
+def index(request):
+        return render(request, 'index.html')
+
+
